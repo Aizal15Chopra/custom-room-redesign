a1fb06ea2c2746f66a8b15fb075ac637776110eb
Aizal15Chopra <99105876+Aizal15Chopra@users.noreply.github.com>
2025-01-23 19:25:19 -0700

    Enhance EmptyState component with a placeholder image and call-to-action button

--- app/dashboard/_components/EmptyState.jsx
+++ app/dashboard/_components/EmptyState.jsx
@@ -1,9 +1,16 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 function EmptyState() {
   return (
-    <div>EmptyState</div>
+    <div className='flex flex-col items-center justify-center mt-10'>
+        <Image src = {'/placeholder.png'} 
+        width = {250} height = {300} alt='image'/>
+        <h2 className='font-medium text-lg text-gray-500 p-5'> Create New AI Interior Design for your room</h2>
+        <Button className='mt-5'>+ Redesign Room</Button>
+    </div>
   )
 }
 
-export default EmptyState
+export default EmptyState 
