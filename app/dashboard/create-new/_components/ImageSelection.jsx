9f118a044251275b5d6536c14a2bb48f8ea1c850
Aizal15Chopra <99105876+Aizal15Chopra@users.noreply.github.com>
2025-01-23 22:24:17 -0700

    Enhance CreateNew page with improved image selection functionality and updated description text

--- app/dashboard/create-new/_components/ImageSelection.jsx
+++ app/dashboard/create-new/_components/ImageSelection.jsx
@@ -1,8 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 function ImageSelection() {
   return (
-    <div>ImageSelection</div>
+    <div>
+        <label>
+            Select Image of your room
+        </label>
+        <div>
+            <label 
+                htmlFor = 'upload-image'>
+                <div>
+                    <Image src = {'/imageupload.png'} width={50} height={50} alt='image'/>
+                </div>
+            </label>
+            <input type='file' accept = 'image/*' 
+            id = 'upload-image' 
+            style = {{display: 'none'}}
+            
+            />
+        </div>
+    </div>
   )
 }
 
--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -6,10 +6,11 @@ function CreateNew() {
     <div>
        <h2 className='font-bold text-4xl text-primary text-center'>Experience the Magic of AI Remodelling</h2>
        <p className='text-center text-lg text-gray-500 mt-2'>
-        Transform any room with a click. Select a space, choose a style, as AI instantly reimagines your environment.
+        Transform any room with a click! Select a space, choose a style, as AI instantly reimagines your environment.
        </p>
 
-       <div>
+       <div className='grid grid-cols-1 md:grid-cols-2 items-center justify-center
+       mt-10'>
         {/* Image Selection*/}
             <ImageSelection />
         {/* Form Input section*/}
