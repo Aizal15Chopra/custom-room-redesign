142c56cf5d2cc08714634dc902f502db136a1d8d
Aizal15Chopra <99105876+Aizal15Chopra@users.noreply.github.com>
2025-01-23 22:36:28 -0700

    Enhance ImageSelection component with file selection functionality and improved styling

--- app/dashboard/create-new/_components/ImageSelection.jsx
+++ app/dashboard/create-new/_components/ImageSelection.jsx
@@ -1,7 +1,13 @@
+"use client"
 import Image from 'next/image'
 import React from 'react'
 
 function ImageSelection() {
+
+    const onFileSelected=(event)=>{
+        console.log(event.target.files[0])
+
+    }
   return (
     <div>
         <label>
@@ -10,13 +16,15 @@ function ImageSelection() {
         <div className='mt-3'>
             <label 
                 htmlFor = 'upload-image'>
-                <div className='p-28 border rounded-xl border-dotted flex justify-center'>
+                <div className='p-28 border rounded-xl 
+                border-dotted flex justify-center border-primary bg-slate-200 cursor-pointer hover:shadow-lg'> 
                     <Image src = {'/imageupload.png'} width={50} height={50} alt='image'/>
                 </div>
             </label>
             <input type='file' accept = 'image/*' 
             id = 'upload-image' 
             style = {{display: 'none'}}
+            onChange={onFileSelected}
             
             />
         </div>
