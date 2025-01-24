6e1a4dc591a1f1dea2a617323c7af41e996e9977
Aizal15Chopra <99105876+Aizal15Chopra@users.noreply.github.com>
2025-01-23 20:57:24 -0700

    Enhance CreateNew page with a title and description for AI remodeling feature

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 function CreateNew() {
   return (
     <div>
-        CreateNew
+       <h2 className='font-bold text-4xl text-primary text-center'>Experience the Magic of AI Remodelling</h2>
+       <p className='text-center text-lg text-gray-500 mt-2'>
+        Transform any room with a click. Select a space, choose a style, as AI instantly reimagines your environment.
+       </p>
 
+       <div>
+        {/* Image Selection*/}
+
+        {/* Form Input section*/}
+
+       </div>
     </div>
   )
 }
