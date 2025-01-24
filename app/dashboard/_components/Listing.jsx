c7352e26549d65297e37c93632403619725286a3
Aizal15Chopra <99105876+Aizal15Chopra@users.noreply.github.com>
2025-01-23 18:54:06 -0700

    Update greeting in Listing component to use user's name instead of full name

--- app/dashboard/_components/Listing.jsx
+++ app/dashboard/_components/Listing.jsx
@@ -1,12 +1,32 @@
 "use client"
+import { Button } from '@/components/ui/button';
 import { useUser } from '@clerk/clerk-react'
-import React from 'react'
+import React, { useState } from 'react'
+import EmptyState from './EmptyState';
 
 function Listing() {
   const {user} = useUser(); 
+  const [userRoomList, setUserRoomList] = useState([]);
   return (
     <div>
-      <h2 className='font-bold text-3xl'> Hello, {user?.fullName} </h2>
+      
+      <div className='flex justify-between items-center'>
+          <h2 className='font-bold text-3xl'> Hello, {user?.fullName} </h2>
+         <Button>+ Redesign Room</Button>
+      </div>
+
+      {userRoomList?.length === 0?
+        <EmptyState/>
+          :
+        <div>
+          {/*Listing*/}
+        </div>
+      
+      
+      }
+
+
+
     </div>
   )
 }
