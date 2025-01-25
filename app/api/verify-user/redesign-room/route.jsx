917d13f40db2838bb259ab583a64fb4167752d51
Aizal15Chopra <99105876+Aizal15Chopra@users.noreply.github.com>
2025-01-24 22:01:42 -0700

    Update verify-user API to use dynamic image URL and prompt for room redesign functionality

--- app/api/verify-user/redesign-room/route.jsx
+++ app/api/verify-user/redesign-room/route.jsx
@@ -10,22 +10,17 @@ export async function POST(req){
 
     try{
         const input = {
-            image: "https://replicate.delivery/pbxt/KhTNuTIKK1F1tvVl8e7mqOlhR3z3D0SAojAMN8BNftCvAubM/bedroom_3.jpg",
-            prompt: "A bedroom with a bohemian spirit centered around a relaxed canopy bed complemented by a large macrame wall hanging. An eclectic dresser serves as a unique storage solution while an array of potted plants brings life and color to the room"
+            image: imageUrl,
+            prompt: "A '+roomType+' room with a '+DesignType+' style interior. '+additionalReq+'"
         };
         
         const output = await replicate.run("adirik/interior-design:76604baddc85b1b4616e1c6475eca080da339c8875bd4996705440484a6eac38", { input });
         console.log(output);
+        return NextResponse.json({result: output})
 
     }catch(e){
+        return NextResponse.json({error: e})
 
     }
 
-
-
-
-
-
-
-    return NextResponse.json({result: "Hello World!"})
 }
